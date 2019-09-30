f801cce7dac8cebfa9de998fe50595b64194e957
Karthik Priya Narayanasamy <knarayanasamy@horizon.csueastbay.edu>
2019-09-29 18:34:49 -0700

    Modify the content to include mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,20 +11,21 @@ router.get('/', function(req, res, next) {
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
 router.get('/mongodb', function (request, response, next) {
-  mongodb.MongoClient.connect("mongodb://karthikpriya:Db@pass1@ds157223.mlab.com:57223/heroku_7386fvlz", function(err, db) {
+  MongoClient.connect("mongodb://karthikpriya:Db%40pass1@ds157223.mlab.com:57223/heroku_7386fvlz", function(err, database) {
     if(err) throw err;
     //get collection of routes
-    var Routes = db.collection('Routes');
+    const myAwesomeDB = database.db('heroku_7386fvlz')
+    var Routes = myAwesomeDB.collection('Routes');
     //get all Routes with frequency >=1
     Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
       if(err) throw err;
 
-      response.render('pages/mongodb', {results: docs});
+      response.render('mongodb', {results: docs});
 
     });
 
     //close connection when your app is terminating.
-    db.close(function (err) {
+    database.close(function (err) {
       if(err) throw err;
     });
   });//end of connect
