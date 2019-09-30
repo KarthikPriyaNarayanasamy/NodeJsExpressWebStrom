4eaa6044538d48d080acf8ddef56a252141c70ee
Karthik Priya Narayanasamy <knarayanasamy@horizon.csueastbay.edu>
2019-09-29 18:19:52 -0700

    Modify the content to include mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello how are u ' });
 });
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response, next) {
+  mongodb.MongoClient.connect("mongodb://karthikpriya:Db@pass1@ds157223.mlab.com:57223/heroku_7386fvlz", function(err, db) {
+    if(err) throw err;
+    //get collection of routes
+    var Routes = db.collection('Routes');
+    //get all Routes with frequency >=1
+    Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+      if(err) throw err;
+
+      response.render('pages/mongodb', {results: docs});
+
+    });
+
+    //close connection when your app is terminating.
+    db.close(function (err) {
+      if(err) throw err;
+    });
+  });//end of connect
+});//end app.get
 
 module.exports = router;
